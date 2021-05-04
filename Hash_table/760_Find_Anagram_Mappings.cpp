0b074e5f4f1d11e2a008779818dc11c27ffc3991
sivat046 <83492302+sivat046@users.noreply.github.com>
2021-05-04 22:01:55 +0530

    Frequecy: 0 %
    https://leetcode.com/problems/find-anagram-mappings/

---
+++ Hash_table/760_Find_Anagram_Mappings.cpp
@@ -0,0 +1,45 @@
+// Question:
+
+// Given two lists Aand B, and B is an anagram of A. B is an anagram of A means B is made by randomizing the order of the elements in A.
+
+// We want to find an index mapping P, from A to B. A mapping P[i] = j means the ith element in A appears in B at index j.
+
+// These lists A and B may contain duplicates. If there are multiple answers, output any of them.
+
+// For example, given
+// A = [12, 28, 46, 32, 50]
+// B = [50, 12, 32, 46, 28]
+
+// We should return
+// [1, 4, 3, 2, 0]
+
+// as P[0] = 1 because the 0th element of A appears at B[1], and P[1] = 4 because the 1st element of A appears at B[4], and so on.
+
+// Note:
+
+//     A, B have equal lengths in range [1, 100].
+//     A[i], B[i] are integers in range [0, 10^5].
+
+class Solution {
+public:
+    vector<int> anagramMappings(vector<int>& A, vector<int>& B) 
+    {
+        unordered_map<int,int> m;
+         for(int i=0;i<B.size();i++)
+         {
+            m[B[i]]=i;
+         }
+        
+        for(int i=0;i<A.size();i++)
+        {
+            if(m.find(A[i]) != m.end())
+            {
+                B[i]=m[A[i]];
+            }
+        }
+        return B;
+    }
+};
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Find Anagram Mappings.
+// Memory Usage: 8.7 MB, less than 48.86% of C++ online submissions for Find Anagram Mappings.
