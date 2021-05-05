99d47bf801d89f7edb5ff50873a45143e905bc86
sivat046 <83492302+sivat046@users.noreply.github.com>
2021-05-05 13:32:25 +0530

    Frequency: 30%
    https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/

---
+++ Hash_table/1160_Find_Words_That_Can_Be_Formed_by_Characters.cpp
@@ -0,0 +1,55 @@
+// Question: Find Words That Can Be Formed by Characters
+
+// You are given an array of strings words and a string chars.
+
+// A string is good if it can be formed by characters from chars (each character can only be used once).
+
+// Return the sum of lengths of all good strings in words.
+
+ 
+
+// Example 1:
+// Input: words = ["cat","bt","hat","tree"], chars = "atach"
+// Output: 6
+// Explanation: 
+// The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+  
+// Example 2:
+// Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+// Output: 10
+// Explanation: 
+// The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+
+// Note:
+
+// 1 <= words.length <= 1000
+// 1 <= words[i].length, chars.length <= 100
+// All strings contain lowercase English letters only.
+
+class Solution {
+public:
+    int countCharacters(vector<string>& W, string C) {
+        unordered_map<char,int> m,p;
+        int k=0,sum=0;
+        for(int i=0;i<C.size();i++) m[C[i]]++;
+        
+        for(string x:W)
+        {
+            p=m;
+            k=1;
+            for(int i=0;i<x.size();i++)
+            {
+                p[x[i]]--;
+                if(p[x[i]] <0) 
+                {
+                    k=0;
+                    break;
+                }
+            }
+            if(k>0)  sum+=x.size();
+        }
+        
+        return sum;
+    }
+};
